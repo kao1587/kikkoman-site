a79fcb97ca3d7ac174bbce7411b30feab9c87d67
kao <25aw0114@jec.ac.jp>
2025-11-26 17:44:09 +0900

    1126 submit btn

---
+++ kikkoman-site/src/components/SubmitBtn.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../styles/Submitbtn.module.css';
+import usePageScroll from '../hooks/usePageScroll';
+
+const SubmitBtn = () => {
+  const scrollDirection = usePageScroll();
+  const btnClass = `
+    ${scrollDirection === 'down' ? styles["btn-show"] : ''}
+  `;
+  return (
+    <button type='button' className={`${styles["submit-btn"]} ${btnClass} shadow`}>今すぐ<br />投稿する</button>
+  )
+}
+
+export default SubmitBtn
--- kikkoman-site/src/pages/Home.jsx
+++ kikkoman-site/src/pages/Home.jsx
@@ -1,35 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import cookHatIcon from '../assets/mdi_cook.svg';
 import TopSlideCard from "../components/TopSlideCard";
 import styles from '../styles/Home.module.css';
+import SubmitBtn from '../components/SubmitBtn.jsx';
 
 function Home() {
-  const [viewPortWidth, setViewPortWidth] = useState(window.innerWidth);
-  const [elementHeight, setElementHeight] = useState(0);
-  const topElementRef = useRef(null);
-
-  const updateElementHeight = () => {
-    if (topElementRef.current) {
-      setElementHeight(topElementRef.current.offsetHeight);
-    }
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      setViewPortWidth(window.innerWidth);
-      updateElementHeight();
-    };
-
-    updateElementHeight();
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
-    <div className={styles["top"]} ref={topElementRef}>
+    <div className={styles["top"]}>
       <div className={`${styles["top-container"]} container`}>
         <h1>みんなの時短レシピ</h1>
         <div className={styles["title-description"]}>
@@ -39,11 +16,7 @@ function Home() {
         <TopSlideCard />
         <p className={styles["top-description"]}>ママの笑顔が、おいしさの秘訣！<br />忙しいママたちがシェアする時短レシピで、<br />短時間で家族に美味しい笑顔を<wbr />届けましょう。</p>
       </div>
-      <div className={styles["top-background"]}>
-        <svg xmlns="http://www.w3.org/2000/svg" width={viewPortWidth} height={elementHeight} viewBox={`0 0 ${viewPortWidth} 844`} fill="none">
-          <path d="M0 64.4242C0 64.4242 314.311 -80.5302 512 64.4242C709.689 209.379 1024 123.03 1024 123.03V758.244C1024 758.244 856.178 951.195 512 758.244C167.822 565.292 0 758.244 0 758.244V64.4242Z" fill="white" />
-        </svg>
-      </div>
+      <SubmitBtn />
     </div>
 
   );
